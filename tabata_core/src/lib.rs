e83c6904c2bfc69a4781632e10fb1d9087ce0866
Aaron Lelouvier <aaron.lelouvier@protonmail.com>
2025-04-18 10:07:06 +0200

    Smoother timesteps.

--- tabata_core/src/lib.rs
+++ tabata_core/src/lib.rs
@@ -2,7 +2,7 @@ use embedded_graphics::{
     mono_font::{ascii::FONT_6X9, MonoTextStyle},
     pixelcolor::BinaryColor,
     prelude::*,
-    primitives::{circle, Circle, PrimitiveStyleBuilder},
+    primitives::{Circle, PrimitiveStyleBuilder},
     text::Text,
 };
 
@@ -16,8 +16,8 @@ pub enum Direction {
 pub struct TabataState {
     // previous_time: u64,
     // button_is_pressed: bool,
-    pub remaining_time: u64,
-    pub total_time: u64,
+    pub remaining_time_ms: u64,
+    pub total_time_ms: u64,
 }
 
 pub fn update_display<D>(display: &mut D, state: &TabataState) -> Result<(), D::Error>
@@ -28,7 +28,7 @@ where
     let text_style: MonoTextStyle<'_, <D as DrawTarget>::Color> = MonoTextStyle::new(&FONT_6X9, D::Color::from(BinaryColor::On));
     let max_radius = 100;
 
-    let progress = (state.total_time - state.remaining_time) as f32 / state.total_time as f32;
+    let progress = (state.total_time_ms - state.remaining_time_ms) as f32 / state.total_time_ms as f32;
     let radius = (progress * max_radius as f32) as i32;
 
     let size = display.bounding_box().size;
@@ -47,7 +47,7 @@ where
         .draw(display)?;
 
     // Draw the timer text
-    let time_text = format!("{:02}:{:02}", state.remaining_time / 60, state.remaining_time % 60);
+    let time_text = format!("{:02}:{:02}", state.remaining_time_ms / 1000/ 60, (state.remaining_time_ms / 1000) % 60);
     Text::new(
         &time_text,
         Point::new(center.x - 18, center.y - 10),
--- tabata_simulator/src/main.rs
+++ tabata_simulator/src/main.rs
@@ -2,20 +2,15 @@ use embedded_graphics_simulator::{
     BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, Window,
 };
 use std::thread::sleep;
-use std::time::Duration;
-use tabata_core::embedded_graphics::{
-    mono_font::{MonoTextStyle, ascii::FONT_6X9},
-    pixelcolor::BinaryColor,
-    prelude::*,
-    primitives::{Circle, PrimitiveStyleBuilder},
-    text::Text,
-};
+use std::time::{SystemTime, Duration};
 
+use tabata_core::embedded_graphics::{pixelcolor::BinaryColor, prelude::*};
 use tabata_core::{TabataState, update_display};
 
 const WIDTH: u32 = 240;
 const HEIGHT: u32 = 320;
-const TIMER_DURATION: u64 = 5;
+const TIMER_DURATION_MS: u64 = 5000;
+const TIMER_STEP_MS: u64 = 100;
 
 fn main() -> Result<(), core::convert::Infallible> {
     let output_settings = OutputSettingsBuilder::new()
@@ -26,16 +21,23 @@ fn main() -> Result<(), core::convert::Infallible> {
     let mut display = SimulatorDisplay::<BinaryColor>::new(Size::new(WIDTH, HEIGHT));
 
     let mut state = TabataState {
-        remaining_time: 0,
-        total_time: TIMER_DURATION,
+        remaining_time_ms: 0,
+        total_time_ms: TIMER_DURATION_MS,
     };
-    for remaining_time in (0..=TIMER_DURATION).rev() {
-        state.remaining_time = remaining_time;
-
+    for remaining_time in (0..=TIMER_DURATION_MS)
+        .rev()
+        .step_by(TIMER_STEP_MS as usize)
+    {
+        let target_time = SystemTime::now() + Duration::from_millis(TIMER_STEP_MS);
+        
+        state.remaining_time_ms = remaining_time;
         let _ = update_display(&mut display, &state);
-
         window.update(&display);
-        sleep(Duration::from_secs(1));
+        
+        let current_time = SystemTime::now();
+        if let Ok(duration) = target_time.duration_since(current_time) {
+            sleep(duration);
+        }
     }
 
     Ok(())
